13c2fc298f9bb8c2a2b44924359bbcfb86f18f7f
yanisse-saddek <yanisse321@gmail.com>
2022-02-24 14:47:51 +0100

    exercice finito + bonus

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -10,14 +10,13 @@ export class Box extends React.Component{
             return <input name="heart" type="range" min={this.props.min} max={this.props.max} value={this.props.valeur} onInput={this.props.onChange}></input>
         }
         if(this.props.icon == "directions_walk"){
-            return <input name="directions_walk" type="range" min={this.props.min} max={this.props.max} value={this.props.valeur} onInput={this.props.onChange}></input>
+            return <input step="1000" name="directions_walk" type="range" min={this.props.min} max={this.props.max} value={this.props.valeur} onInput={this.props.onChange}></input>
         }
         if(this.props.icon == "wb_sunny"){
             return <input name="temperature" type="range" min={this.props.min} max={this.props.max} value={this.props.valeur} onInput={this.props.onChange}></input>
         }
     }
     render(){
-
         return(
             <div className="box col-sm-3 col-6">
                 <span style={{fontSize:'100px', color:this.props.color}} className="material-icons">
